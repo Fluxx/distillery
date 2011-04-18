d957a051164f8530c2596f040c78c86c736381b9
Jeff Pollard <jeff.pollard@gmail.com>
2011-04-17 20:59:57 -0700

    Remove debug from the spec config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
   config.color_enabled = true
-  config.debug = true
 
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
