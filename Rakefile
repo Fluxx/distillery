bdd0e876110b324fa5f541e3d79e24ded0c7e5d6
Jeff Pollard <jeff.pollard@gmail.com>
2011-04-17 18:14:49 -0700

    Rake tasks setup for rspec.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require 'bundler'
 Bundler::GemHelper.install_tasks
+
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.rspec_opts = %w[--profile]
+  t.pattern = 'spec/**/*_spec.rb'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'distillery'
+require 'rspec'
+
+Dir['./spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.debug = true
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
