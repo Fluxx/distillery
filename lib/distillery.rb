38828c427823045bbb092df5f99dd6697e367929
Jeff Pollard <jeff.pollard@gmail.com>
2011-04-28 23:22:46 -0700

    Add Distillery.distill method as sugar to simply distill documents

--- lib/distillery.rb
+++ lib/distillery.rb
@@ -2,4 +2,8 @@ require "distillery/document"
 
 module Distillery
   ROOT = File.dirname(__FILE__)
+
+  def self.distill(str, options = {})
+    Document.new(str).distill!(options)
+  end
 end
---
+++ spec/lib/distillery_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Distillery do
+
+  describe '.distill' do
+
+    let(:document) { File.open('./spec/fixtures/pina_collada_cupcakes.html').read }
+    let(:mockdoc) { mock(:doc, :distill => 'test') }
+
+    it 'takes a string and returns the distilled markup' do
+      Distillery.distill(document).should be_a(String)
+    end
+
+    it 'defers to Distillery::Document' do
+      Distillery::Document.should_receive(:new).once.with(document).and_return(mockdoc)
+      mockdoc.should_receive(:distill!).once
+      Distillery.distill(document)
+    end
+
+    it 'passes the same options through to the distill! method' do
+      Distillery::Document.stub!(:new).and_return(mockdoc)
+      mockdoc.should_receive(:distill!).once.with(hash_including(:dirty => true))
+      Distillery.distill(document, :dirty => true)
+    end
+  end
+
+end
