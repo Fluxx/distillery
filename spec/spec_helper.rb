7ce25732cd51135826071ac1dc538aebe2f329f9
Jeff Pollard <jeff.pollard@gmail.com>
2011-04-30 20:58:33 -0700

    Add debug to rspec option.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 require 'distillery'
 require 'rspec'
+require 'ruby-debug'
 
 Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 RSpec.configure do |config|
   config.color_enabled = true
+  config.debug = true
 
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
